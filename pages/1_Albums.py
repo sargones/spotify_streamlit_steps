c34d2e7bcd951030a960c0b77ac04497ca8d1414
sargones <7fac3ae7@opayq.com>
2024-10-30 19:32:30 +0000

    new files

---
+++ pages/1_Albums.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+from main import albums_df
+
+
+
+st.set_page_config(page_title="Albums details", page_icon="📈")
+
+#albums_df = pd.read_csv('/home/pipi/PycharmProjects/spotify_streamlit/albums.csv')
+albums_df = albums_df.drop(columns=['artist_id', 'album_type'])
+st.title('Album details per band')
+
+#drop_down on Band
+bands = albums_df['artist_name'].drop_duplicates()
+band = st.selectbox('Select a band', bands, index= None)
+try:
+    if len(band)>0:
+        band_scope = albums_df['artist_name']==band
+except:
+    band_scope = albums_df['artist_name'].isin(list(bands))
+
+# slider for the years
+start_year, end_year = st.slider("Select years", 1990, 2024, (2000, 2020))
+
+st.dataframe(albums_df[(band_scope) & ((albums_df['release_date']>=start_year) & (albums_df['release_date']<=end_year) )],
+                    hide_index=True, column_order=('artist_name', 'name', 'release_date', 'total_tracks'),
+                    column_config = {'release_date': st.column_config.NumberColumn('release_date', format='%d')},
+                    width = 800)
+
---
+++ pages/2_Songs.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+from main import songs_df
+
+
+st.set_page_config(page_title="Top 10 songs details", page_icon="📈")
+
+#albums_df = pd.read_csv('/home/pipi/PycharmProjects/spotify_streamlit/albums.csv')
+songs_df = songs_df.drop(columns=['id'])
+songs_year = songs_df[['artist','song_name','song_album','song_release_year']]
+
+st.title('Top 10 songs details')
+st.subheader("Songs release date")
+#drop_down on Band
+bands = songs_year['artist'].drop_duplicates()
+band = st.selectbox('Select a band', bands, index= None)
+try:
+  if len(band)>0:
+    band_scope = songs_year['artist']==band
+except:
+  band_scope = songs_year['artist'].isin(list(bands))
+
+# slider for the years
+min_year = songs_year['song_release_year'].min()
+max_year = songs_year['song_release_year'].max()
+start_year, end_year = st.slider("Select years", min_year, max_year, (2010, 2024))
+
+st.dataframe(songs_year[(band_scope) & ((songs_year['song_release_year']>=start_year) & (songs_year['song_release_year']<=end_year) )],
+                    hide_index=True, column_config = {'song_release_year': st.column_config.NumberColumn('song_release_year', format='%d')},
+                    width = 800)
+
+st.divider()
+st.subheader("Songs popularity")
+
+popularity_df = songs_df[['artist','song_name','song_album','song_popularity']]
+min_pop = popularity_df['song_popularity'].min()
+max_pop = popularity_df['song_popularity'].max()
+
+
+low_pop, high_pop = st.slider("Select a range", min_pop, max_pop, (min_pop + 10, max_pop - 10))
+
+st.dataframe(popularity_df[(band_scope) & ((popularity_df['song_popularity']>=low_pop) & (popularity_df['song_popularity']<=high_pop))],
+             hide_index=True, width = 800)
